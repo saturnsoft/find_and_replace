4fce6ecb29ed7a6675e9c3e97bfeb297c8ba2a30
szabolcs.suranyi@capgemini.com <szabolcs.suranyi@capgemini.com>
2023-10-10 11:49:37 +0200

    BFS implemented

--- word_trans.py
+++ word_trans.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+#import queue
+from collections import deque
+
+def shift(w, i):
+    ww = ""
+    for c in w:
+        ww += chr(((ord(c) - ord("a") + i) % 26) + ord("a"))
+    return ww
+
 def apply_rule(w: str, r: tuple) -> str:
     if r[0] == "r":
         return w.replace(r[1], r[2])
@@ -9,7 +18,31 @@ def apply_rule(w: str, r: tuple) -> str:
         return w[1:] + w[0]
     elif r[0] == "rr":
         return w[-1] + w[:-1]
+    elif r[0] == "i":
+        return shift(w, 1)
+    elif r[0] == "d":
+        return shift(w, -1)
 
-w1 = "abcd"
+w1 = "abcdz"
+w2 = "bcde"
 
-print(apply_rule(w1, ("rr",) ))
+rules = [ ("i",), ("rr",), ("r","za","a") ]
+
+states = deque()
+
+states.append( (w1, []) )
+
+while len(states) > 0:
+    state = states.popleft()
+    print("Checking state: {}".format(state))
+    for r in rules:
+        if r not in state[1]:
+            w = apply_rule(state[0], r)
+            applied_states = state[1] + [r]
+
+            if w == w2:
+                print("Found solution: {}".format(applied_states))
+
+            states.append( (w, applied_states) )
+
+#print(apply_rule(w1, ("d",) ))
