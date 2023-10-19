ecd73e9efeb57e8b192ca45e777ca178c212bde3
Szabolcs Suranyi <suranyi.szabolcs@gmail.com>
2023-10-19 14:52:41 +0200

    fixed curses

--- game01.py
+++ game01.py
@@ -1,30 +1,56 @@
-#import keyboard
-from getkey import getkey
+import curses
 
 def toggle_lock(c: str) -> str:
     return "X" if c == "." else "."
 
-word = "applepie"
+def add_char(w: str, i: int) -> str:
+    ww = ""
+    for c in w:
+        ww += chr(((ord(c) - ord("a") + i) % 26) + ord("a"))
+    return ww
+
+theword = "bear"
+
+word = theword
 lock = "." * len(word)
 
-print(word)
-print(lock)
+window = curses.initscr()
+curses.noecho()
+window.keypad(True)
+curses.curs_set(0)
 
 while True:
-    #c = keyboard.read_key()
+    k = window.get_wch()
 
-    
-    if c == "q":
-        break
-    elif c.isnumeric():
-        i = ord(c[0]) - ord("0")
+    if type(k) == str:
+        if k == "q":
+            break
+        elif k.isnumeric():
+            i = ord(k[0]) - ord("0")
+            lock = lock[:i] + toggle_lock(lock[i]) + lock[i+1:]
+        elif k == "a":
+            word = word[1:] + word[0]
+        elif k == "d":
+            word = word[-1] + word[:-1]
+        elif k == "w":
+            w = ""
+            for i, c in enumerate(word):
+                if lock[i] != "X":
+                    w += add_char(c, 1)
+                else:
+                    w += c
+            word = w
+        elif k == "s":
+            w = ""
+            for i, c in enumerate(word):
+                if lock[i] != "X":
+                    w += add_char(c, -1)
+                else:
+                    w += c
+            word = w
+        elif k == "r":
+            word = theword
 
-        l1 = lock[:i]
-        l2 = toggle_lock(lock[i])
-        l3 = lock[i+1:]
-        
-        lock = lock[:i] + toggle_lock(lock[i]) + lock[i+1:]
-    
-    print(word)
-    print(lock)
+    window.addstr(1, 1, word)
+    window.addstr(2, 1, lock)
 
